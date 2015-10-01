645bb5ef1845a20a797c1451502c05e948074ad4
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-10-01 14:38:32 +1000

    reconstruct for one iteration

--- irls.cpp
+++ irls.cpp
@@ -3,25 +3,24 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <vector>
 #include <Eigen/Dense>
 using namespace std;
 using namespace Eigen;
 
-float madsigma(VectorXf residuals, int rank)
+float madsigma(VectorXcf residuals, int rank)
 {
     float sigma = 0.6745;
-    residuals = residuals.array().abs();
-    sort(residuals.data(), residuals.data()+residuals.size());
-    int middle = (rank - 1 + residuals.size()) / 2;
-    float median = residuals(middle);
+    VectorXf residual_magnitues = residuals.array().abs();
+    sort(residual_magnitues.data(), residual_magnitues.data()+residual_magnitues.size());
+    int middle = (rank - 1 + residual_magnitues.size()) / 2;
+    float median = residual_magnitues(middle);
     return median / sigma;
 }
 
-VectorXf tukey_biweight(VectorXf residuals)
+VectorXf tukey_biweight(VectorXcf residuals)
 {
     VectorXf abs_weights = residuals.array().abs();
-    VectorXf weights = residuals;
+    VectorXf weights = residuals.array().abs();
     for (int i = 0; i < weights.size(); i++) {
         if (abs_weights(i) > 1) {
             weights(i) = 0;
@@ -30,26 +29,26 @@ VectorXf tukey_biweight(VectorXf residuals)
     return (1 - weights.array().square()).square();
 }
 
-VectorXf irls(MatrixXf x, VectorXf y)
+VectorXcf irls(MatrixXcf x, VectorXcf y)
 {
     int n = x.rows();
     int p = x.cols();
 
-    VectorXf prior_weights = VectorXf::Ones(n);
-    VectorXf curr_weights = VectorXf::Ones(n);
-    VectorXf new_weights;
+    VectorXcf prior_weights = VectorXcf::Ones(n);
+    VectorXcf curr_weights = VectorXcf::Ones(n);
+    VectorXcf new_weights;
 
-    ColPivHouseholderQR<MatrixXf> qr = x.colPivHouseholderQr();
-    VectorXf b = qr.solve(y);
-    VectorXf b0;
+    ColPivHouseholderQR<MatrixXcf> qr = x.colPivHouseholderQr();
+    VectorXcf b = qr.solve(y);
+    VectorXcf b0;
 
-    MatrixXf r = qr.matrixR().triangularView<Upper>();
+    MatrixXcf r = qr.matrixR().triangularView<Upper>();
     // B/A = (A'\B')'
-    MatrixXf e =  r.transpose().colPivHouseholderQr().
+    MatrixXcf e =  r.transpose().colPivHouseholderQr().
         solve((x * qr.colsPermutation()).transpose()).
         transpose();
-    VectorXf h = (e.array().square()).rowwise().sum();
-    VectorXf adj_factor = 1 / (1 - h.array() / prior_weights.array()).sqrt();
+    VectorXcf h = (e.array().square()).rowwise().sum();
+    VectorXcf adj_factor = 1 / (1 - h.array() / prior_weights.array()).sqrt();
 
     //cout << "E" << endl;
     //cout << e << endl << endl;
@@ -73,7 +72,7 @@ VectorXf irls(MatrixXf x, VectorXf y)
     float D = 1.4901e-08;
 
     int iter_limit = 50;
-    VectorXf res, r_adj;
+    VectorXcf res, r_adj;
     for (int i = 0; i < iter_limit; i++) {
         if (b0.size()) {
             // test threshhold
@@ -87,8 +86,8 @@ VectorXf irls(MatrixXf x, VectorXf y)
         VectorXf weights = tukey_biweight(r_adj.array() / (s * tune));
         b0 = b;
         weights = weights.array().sqrt();
-        VectorXf y_weighted = y.array() * weights.array();
-        MatrixXf x_weighted = x.array() * (weights * VectorXf::Ones(p).transpose()).array();
+        VectorXcf y_weighted = y.array() * weights.array();
+        MatrixXcf x_weighted = x.array() * (weights * VectorXcf::Ones(p).transpose()).array();
         qr = x_weighted.colPivHouseholderQr();
         wxrank = qr.rank();
         b = qr.solve(y_weighted);
@@ -97,9 +96,9 @@ VectorXf irls(MatrixXf x, VectorXf y)
     return b;
 }
 
-MatrixXf loadMatrix(string file_name, int m, int n)
+MatrixXcf loadMatrix(string file_name, int m, int n)
 {
-    MatrixXf a = MatrixXf::Zero(m, n);
+    MatrixXcf a = MatrixXcf::Zero(m, n);
     string line;
     ifstream f2 (file_name);
     int i = 0;
@@ -120,9 +119,9 @@ MatrixXf loadMatrix(string file_name, int m, int n)
     return a;
 }
 
-VectorXf loadVector(string file_name, int m)
+VectorXcf loadVector(string file_name, int m)
 {
-    VectorXf b = VectorXf::Zero(m);
+    VectorXcf b = VectorXcf::Zero(m);
     string line;
     ifstream f2 (file_name);
     int i = 0;
@@ -190,26 +189,26 @@ VectorXcf loadComplexVector(string file_name, int m)
     return b;
 }
 
-int _main()
-{
+//int _main()
+//{
 
-    MatrixXf a = loadMatrix("data/F2_real.csv", 402, 120);
-    VectorXf y = loadVector("data/y_real.csv", 402);
+    //MatrixXcf a = loadMatrix("data/F2_real.csv", 402, 120);
+    //VectorXcf y = loadVector("data/y_real.csv", 402);
 
-    ofstream a_test ("data/a_test");
-    a_test << a;
-    ofstream y_test ("data/y_test");
-    y_test << y;
+    //ofstream a_test ("data/a_test");
+    //a_test << a;
+    //ofstream y_test ("data/y_test");
+    //y_test << y;
     
-    std::clock_t start, end;
-    start = std::clock();
-    VectorXf b = irls(a, y);
-    end = std::clock();
-    ofstream b_test ("data/b_test");
-    b_test << b;
+    //std::clock_t start, end;
+    //start = std::clock();
+    //VectorXcf b = irls(a, y);
+    //end = std::clock();
+    //ofstream b_test ("data/b_test");
+    //b_test << b;
 
-    cout << "Took: " << (end - start) / (double)(CLOCKS_PER_SEC / 1000) << "ms\n" << endl;
+    //cout << "Took: " << (end - start) / (double)(CLOCKS_PER_SEC / 1000) << "ms\n" << endl;
 
-    return 0;
-}
+    //return 0;
+//}
 
--- reconstruct.cpp
+++ reconstruct.cpp
@@ -25,25 +25,91 @@ VectorXcf extractKnown(VectorXcf spectrum, VectorXcf valid_points)
     return extracted;
 }
 
+VectorXcf buildZeroedPoints(int left_cursor, int right_cursor, int size)
+{
+    VectorXcf left = VectorXcf::Ones(left_cursor);
+    VectorXcf middle = VectorXcf::Zero(size - left_cursor - right_cursor);
+    VectorXcf right = VectorXcf::Ones(right_cursor);
+    VectorXcf joined;
+    joined << left, middle, right;
+    return joined;
+}
+
+int getInvalidStart(VectorXcf valid_points)
+{
+    for (int i = 0; i < valid_points.rows(); i++) {
+        if (valid_points.real()(i) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int getInvalidEnd(VectorXcf valid_points)
+{
+    for (int i = valid_points.rows() - 1; i >= 0; i--) {
+        if (valid_points.real()(i) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 void reconstruct(VectorXcf spectrum, VectorXcf valid_points)
 {
     VectorXcf hamming_320 = loadComplexVector("data/hamming_320.dat", 320);
     VectorXcf invalid_points = VectorXcf::Ones(320).array() - valid_points.array();
     VectorXcf channel_impulse_response_valid_points = VectorXcf::Ones(320);
 
+    int invalid_start = getInvalidStart(valid_points);
+    int invalid_end = getInvalidEnd(valid_points);
+
     float stop_threshold = 0.03;
-    int stop_left = 0;
-    int stop_right = 0;
-    int last_run = 0;
+    bool stop_left = false;
+    bool stop_right = false;
+    bool last_run = false;
     int left_cursor = 100;
     int right_cursor = 220;
     MatrixXcf idft = loadComplexMatrix("data/idft.dat", 320, 320);
     VectorXcf spectrum_windowed = spectrum.array() * hamming_320.array();
     VectorXcf known = extractKnown(spectrum_windowed, valid_points);
 
-    while (!stop_left && !stop_right) {
+    //while (!stop_left || !stop_right || last_run) {
+        //VectorXcf zeroed = buildZeroedPoints(left_cursor, right_cursor, 320);
+        //VectorXcf non_zeroed = VectorXcf::Ones(320).array() - zeroed.array();
 
-    }
+
+        MatrixXcf F1_1 = idft.topLeftCorner(left_cursor, invalid_start);
+        MatrixXcf F1_2 = idft.topRightCorner(left_cursor, idft.cols() - invalid_end - 1);
+        MatrixXcf F1_3 = idft.bottomLeftCorner(idft.rows() - right_cursor, invalid_start);
+        MatrixXcf F1_4 = idft.bottomRightCorner(idft.rows() - right_cursor, idft.cols() - invalid_end - 1);
+        MatrixXcf F1(F1_1.rows() + F1_3.rows(), F1_1.cols() + F1_2.cols());
+        F1 << F1_1, F1_2,
+           F1_3, F1_4;
+
+        MatrixXcf F2_1 = idft.block(0, invalid_start, left_cursor, invalid_end - invalid_start);
+        MatrixXcf F2_2 = idft.block(right_cursor, invalid_start, idft.rows() - right_cursor, invalid_end - invalid_start);
+        MatrixXcf F2(F2_1.rows() + F2_2.rows(), F2_1.cols());
+        F2 << F2_1,
+           F2_2;
+
+        MatrixXcf F3_1 = idft.block(left_cursor, 0, right_cursor - left_cursor, invalid_start);
+        MatrixXcf F3_2 = idft.block(left_cursor, invalid_start, right_cursor - left_cursor, idft.cols() - invalid_end);
+        MatrixXcf F3(F3_1.rows(), F3_1.cols() + F3_2.cols());
+        F3 << F3_1, F3_2;
+
+        MatrixXcf F4 = idft.block(left_cursor, invalid_start, right_cursor - left_cursor, invalid_end - invalid_start);
+
+        VectorXcf y = -F1 * known;
+
+        VectorXcf unknown = irls(F2, y);
+
+
+        //printf("F1_1: %d,%d %dx%d\n", 0, 0, (int)F1_1.rows(), (int)F1_1.cols());
+        //printf("F1_2: %d,%d %dx%d\n", 0, 0, (int)F1_2.rows(), (int)F1_2.cols());
+        //printf("F1_3: %d,%d %dx%d\n", 0, 0, (int)F1_3.rows(), (int)F1_3.cols());
+        //printf("F1_4: %d,%d %dx%d\n", 0, 0, (int)F1_4.rows(), (int)F1_4.cols());
+    //}
 
 
 
